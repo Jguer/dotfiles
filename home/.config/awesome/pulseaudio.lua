76133258483ee1210e92dd1cf07f45ddcc6b5ef5
Jguer <joaogg3@gmail.com>
2017-01-15 00:13:14 +0000

    New pulseaudio funks

--- home/.config/awesome/pulseaudio.lua
+++ home/.config/awesome/pulseaudio.lua
@@ -2,76 +2,71 @@ local io = io
 local tonumber = tonumber
 local tostring = tostring
 local string = string
+local awful = require("awful")
 
 module("pulseaudio")
 
-function volumeUp()
-  local dc = defaultCard()
-  local df = tonumber(dc) + 1
-  local fI = io.popen("pacmd list-sinks | awk '/^\\svolume:/{i++} i=='"..tostring(df).."'{print $5; exit}'")
-  local v = fI:read()
-  vol = tonumber(v:sub(1, -2))
-  fI:close()
-  if vol >= 100 or vol <= 0 then
-    return
-  end
-
-  local f = io.popen("pactl set-sink-volume " ..dc.." +3db")
-  f:close()
+function get_default_card()
+    local d = io.popen([[pacmd stat | awk -F": " '/^Default sink name: /{print $2}']])
+    dc = d:read()
+    d:close()
+    return dc
 end
 
-function volumeDown()
-  local dc = defaultCard()
-  local f = io.popen("pactl set-sink-volume " ..dc.." -3db")
-  f:close()
+function get_volume(dc)
+    local fI = io.popen([[pacmd list-sinks | awk '/^\s+name: /{indefault = $2 == "<']]..dc..[['>"}/^\s+volume: / && indefault {print $5; exit}']])
+    local v = fI:read()
+    fI:close()
+    return tonumber(v:sub(1, -2))
 end
 
-function defaultCard()
-  local d = io.popen("pacmd list-sinks | grep -e '* index' | tail -c 2")
-  dc = d:read()
-  d:close()
-  return dc
+function get_mute(dc)
+    local fI = io.popen([[pacmd list-sinks | awk '/^\s+name: /{indefault = $2 == "<']]..dc..[['>"}/^\s+muted: / && indefault {print $2; exit}']])
+    local mute = fI:read()
+    fI:close()
+    if mute == "yes" then; return true; end
+    return false
 end
 
-function volumeMute()
-  local dc = defaultCard()
-  local g = io.popen("pactl set-sink-mute " ..tostring(dc).." toggle")
-  g:close()
-end
+function volume_change(change)
+    local dc = get_default_card()
+    local vol = get_volume(dc)
 
-function volumeInfo()
-  volmin = 0
-  volmax = 65536
-  local dc = defaultCard()
-  local df = tonumber(dc) + 1
-  -- local f = io.popen("pacmd dump |grep set-sink-volume | sed -n " ..tostring(df).."p")
-  local f = io.popen("pacmd list-sinks | awk '/^\\svolume:/{i++} i=='"..tostring(df).."'{print $5; exit}'")
-  local g = io.popen("pacmd dump |grep set-sink-mute | sed -n " ..tostring(df).."p")
-  local v = f:read()
-  local mute = g:read()
-  local iconI
-
-  if v == "" then
-      return "Vol: N/A% "
-  end
-
-  if mute ~= nil and string.find(mute, "no") then
-    vol = tonumber(v:sub(1, -2))
-    volume = v
-    if vol >= 67 and vol <= 100 then
-      iconI = "🔊"
-    elseif vol >= 33 and vol <= 66 then
-      iconI = "🔉"
-    else
-      iconI = "🔈"
+    if change:sub(1,1) == '-' then; if vol < 0 then; return; end
+    else; if vol > 100 then; return; end
     end
-  else
-    volume = "✕%"
-    iconI = "🔇"
-  end
-  f:close()
-  g:close()
 
-  --return "Volume: "..volume
-  return iconI.." Vol: "..volume.." "
+    awful.spawn("pactl set-sink-volume "..dc.." "..change)
+end
+
+function volume_mute()
+    local dc = get_default_card()
+    awful.spawn("pactl set-sink-mute " ..dc.." toggle")
+end
+
+function volume_info()
+    local dc = get_default_card()
+    local mute = get_mute(dc)
+    local vol = get_volume(dc)
+    local iconI
+
+    if vol == nil then
+        return "Vol: N/A% "
+    end
+
+    if mute == false then
+        if vol >= 67 and vol <= 100 then
+            iconI = "🔊"
+        elseif vol >= 33 and vol <= 66 then
+            iconI = "🔉"
+        else
+            iconI = "🔈"
+        end
+    else
+        volume = "✕"
+        iconI = "🔇"
+    end
+
+    --return "Volume: "..volume
+    return iconI.." Vol: "..vol.."% "
 end
