eb27c278318dc23cf568c34f83390c514d232814
Jguer <joaogg3@gmail.com>
2017-05-10 22:30:27 +0100

    Added subtitler

---
+++ home/.config/mpv/scripts/autosub.lua
@@ -0,0 +1,23 @@
+-- requires subliminal, version 1.0 or newer
+-- default keybinding: b
+-- add the following to your input.conf to change the default keybinding:
+-- keyname script_binding auto_load_subs
+local utils = require 'mp.utils'
+function load_sub_fn()
+    subl = "/usr/bin/subdl" -- use 'which subliminal' to find the path
+    mp.msg.info("Searching subtitle")
+    mp.osd_message("Searching subtitle")
+    t = {}
+    t.args = {subl, mp.get_property("media-title")}
+    res = utils.subprocess(t)
+    if res.status == 0 then
+        mp.commandv("rescan_external_files", "reselect")
+        mp.msg.info("Subtitle download succeeded")
+        mp.osd_message("Subtitle download succeeded")
+    else
+        mp.msg.warn("Subtitle download failed")
+        mp.osd_message("Subtitle download failed")
+    end
+end
+
+mp.add_key_binding("b", "auto_load_subs", load_sub_fn)
