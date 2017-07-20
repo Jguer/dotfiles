8a0d819bce5dcc19d4585fa22a205ff72943994a
Jguer <joaogg3@gmail.com>
2017-07-20 09:55:29 +0100

    Removed main menu

--- home/.config/awesome/rc.lua
+++ home/.config/awesome/rc.lua
@@ -260,7 +260,6 @@ end)
 -- {{{ Mouse bindings
 -- @DOC_ROOT_BUTTONS@
 root.buttons(gears.table.join(
-    awful.button({ }, 3, function () mymainmenu:toggle() end),
     awful.button({ }, 4, awful.tag.viewnext),
     awful.button({ }, 5, awful.tag.viewprev)
 ))
