520e9911c1fe5b7dd580d25f124d5ae6b37b227c
Jguer <me@jguer.space>
2018-02-12 23:13:55 +0000

    fix some awesomewm issues

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -264,8 +264,6 @@ globalkeys = gears.table.join(
     end,
     {description = "focus previous by index", group = "client"}
   ),
-  awful.key({ modkey,           }, "w", function () mymainmenu:show() end,
-    {description = "show main menu", group = "awesome"}),
 
   -- Layout manipulation
   awful.key({ modkey, "Shift"   }, "j", function () awful.client.swap.byidx(  1)    end,
