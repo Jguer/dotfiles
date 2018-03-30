4189933775edf7d335fe665d593a8f84db73c684
Jguer <me@jguer.space>
2018-03-30 19:52:09 +0200

    Changes made according to times
    Signed-off-by: Jguer <me@jguer.space>

--- mpv/scripts/vo_battery.lua
+++ mpv/scripts/vo_battery.lua
@@ -1,6 +1,6 @@
 -- If the laptop is on battery, the VO set in the config will be choosen,
 -- else the one defined with „hqvo“ is used.
-local hqprofile = "high-quality"
+local hqprofile = "low-quality"
 local utils = require 'mp.utils'
 if mp.get_property_bool("option-info/vo/set-from-commandline") == true then
   return
