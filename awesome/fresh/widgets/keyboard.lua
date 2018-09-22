401f4e6bc224066a3e592048568cda7d8e74e639
Jguer <me@jguer.space>
2018-09-22 19:50:58 +0100

    Compton fixes
    Signed-off-by: Jguer <me@jguer.space>

--- awesome/fresh/widgets/keyboard.lua
+++ awesome/fresh/widgets/keyboard.lua
@@ -9,7 +9,7 @@ local recolor_image = gears_color.recolor_image
 local keyboard = { mt = {} }
 
 local style = {
-  width   = 42,
+  width   = 50,
   icon    = beautiful.themes_path .. "widgets/keyboard.svg",
 }
 
