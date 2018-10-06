8ae4ae3fd698d17468bbaf666d0861767740cce5
Jguer <me@jguer.space>
2018-10-06 17:28:08 +0100

    Update autosub lua for dual language. Update gtk settings for chrome os fonts

--- mpv/scripts/autosub.lua
+++ mpv/scripts/autosub.lua
@@ -1,33 +1,26 @@
--- default keybinding: b
+--- default keybinding: b
 local utils = require 'mp.utils'
+local mp = require 'mp'
+
+local subl = "/bin/subdl"
+local lang_list = {"fre","eng"}
+
 function load_sub_fn()
-  subl = "/bin/subdl"
-  path = mp.get_property("path")
-  srt_path = string.gsub(path, "%.%w+$", ".srt")
-  srt_args = "--lang=fre,eng --existing=bypass --output=".. srt_path
-  mp.msg.info("Searching subtitle")
-  mp.osd_message("Searching subtitle")
-  t = {}
-  t.args = {subl, srt_args, path}
-  res = utils.subprocess(t)
-  if res.status == 0 then
-    mp.commandv("rescan_external_files", "reselect")
-    mp.msg.info("Subtitle download succeeded by path")
-    mp.osd_message("Subtitle download succeeded by path")
-  else
-    t.args = {subl,mp.get_property("media-title")}
-    res = utils.subprocess(t)
-    if res.status == 0 then
-      mp.commandv("rescan_external_files", "reselect")
-      mp.msg.info("Subtitle download succeeded by title")
-      mp.osd_message("Subtitle download succeeded by title")
-    else
-      mp.msg.warn("Subtitle download failed")
-      mp.osd_message("Subtitle download failed")
-      t.args = {subl,mp.get_property("title")}
-      res = utils.subprocess(t)
+    local path = mp.get_property("path")
+    mp.osd_message("Searching subtitle")
+    local t = {}
+    for _, lang in ipairs(lang_list) do
+        t.args = {subl, "--existing=bypass",
+        string.format("--lang=%s", lang), "--output={m}.{L}.{S}",path}
+        t.res = utils.subprocess(t)
+        if t.res.error == nil then
+            t.reload = true
+            mp.osd_message("Subtitle download succeeded by path")
+        end
     end
-  end
+    if t.reload then
+        mp.commandv("rescan_external_files", "reselect")
+    end
 end
 
 mp.add_key_binding("b", "auto_load_subs", load_sub_fn)
