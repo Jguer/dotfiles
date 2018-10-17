fddb173f2ddb2802b0bd351f23cb4603e8e48de3
Jguer <me@jguer.space>
2018-10-17 11:46:23 +0100

    Move compton to awesome named autostart. Create zprofile

--- awesome/autostart.lua
+++ awesome/autostart.lua
@@ -38,5 +38,9 @@ autorun["all"] = {
   "unclutter -noevents -idle 2 -jitter 1 -root"
 }
 
+autorun["atreides"] = {
+  "compton --config compton.conf"
+}
+
 run_once(autorun["all"])
 run_once(autorun[hostname] or {})
