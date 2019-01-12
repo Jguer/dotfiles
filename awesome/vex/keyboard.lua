fb535215df568adde068d650fa032b0f4a4699ec
Jguer <me@jguer.space>
2019-01-12 13:07:23 +0000

    Added VPN control widget. Refactor to (mostly) upstream specification

--- awesome/vex/keyboard.lua
+++ awesome/vex/keyboard.lua
@@ -1,50 +1,51 @@
 local setmetatable = setmetatable
-local awful         = require("awful")
-local beautiful     = require("beautiful")
-local widget_base   = require("wibox.widget.base")
-local wibox         = require("wibox")
-local gears_color   = require("gears.color")
+local awful = require("awful")
+local beautiful = require("beautiful")
+local widget_base = require("wibox.widget.base")
+local wibox = require("wibox")
+local gears_color = require("gears.color")
 local recolor_image = gears_color.recolor_image
 
-local keyboard = { mt = {} }
+local keyboard = {mt = {}}
 
 local style = {
-  width   = 41,
-  icon    = beautiful.themes_path  .. "icons/keyboard.svg",
+    width = 41,
+    icon = beautiful.themes_path .. "icons/keyboard.svg"
 }
 
 -- @return A pulse widget.
 function keyboard.new()
-  local icon = wibox.widget {
-    image  = recolor_image(style.icon, beautiful.widget.fg),
-    resize = true,
-    forced_width = 16,
-    forced_height = 16,
-    widget = wibox.widget.imagebox
-  }
+    local icon =
+        wibox.widget {
+        image = recolor_image(style.icon, beautiful.widget.fg),
+        resize = true,
+        forced_width = 16,
+        forced_height = 16,
+        widget = wibox.widget.imagebox
+    }
 
-  local text = awful.widget.keyboardlayout()
+    local text = awful.widget.keyboardlayout()
 
-  local layout = wibox.layout.fixed.horizontal(wibox.container.place(icon), text)
+    local layout = wibox.layout.fixed.horizontal(wibox.container.place(icon), text)
 
-  local widget = wibox.container.constraint(layout, "exact", style.width)
-  local self = widget_base.make_widget(widget)
-  self.icon = icon
-  self.text = text
+    local widget = wibox.container.constraint(layout, "exact", style.width)
+    local self = widget_base.make_widget(widget)
+    self.icon = icon
+    self.text = text
 
-  -- Mouse bindings, for when you want to add GMT change
-  self:buttons(self.text:buttons())
+    -- Mouse bindings, for when you want to add GMT change
+    self:buttons(self.text:buttons())
 
-  return self
+    return self
 end
 
-local _instance = nil;
+local _instance = nil
 
 function keyboard.mt:__call(...)
-  if _instance == nil then
-    _instance = self.new(...)
-  end
-  return _instance
+    if _instance == nil then
+        _instance = self.new(...)
+    end
+    return _instance
 end
 
 return setmetatable(keyboard, keyboard.mt)
