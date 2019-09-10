ae83c7ed107ae5d42de3b77e961fb67d295867fa
Jguer <me@jguer.space>
2019-09-10 10:09:44 +0100

    Make it fedora compatible

--- code/install_code.sh
+++ code/install_code.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
+# Generated using
+# code --list-extensions | xargs -L 1 echo code --install-extension
+
 code --install-extension ahmadawais.shades-of-purple
 code --install-extension bierner.markdown-preview-github-styles
 code --install-extension christian-kohler.path-intellisense
+code --install-extension felipecaputo.git-project-manager
 code --install-extension foxundermoon.shell-format
 code --install-extension Gruntfuggly.todo-tree
+code --install-extension James-Yu.latex-workshop
 code --install-extension mikestead.dotenv
 code --install-extension ms-azuretools.vscode-docker
 code --install-extension ms-python.python
+code --install-extension ms-vscode-remote.remote-containers
 code --install-extension ms-vscode-remote.remote-ssh
 code --install-extension ms-vscode-remote.remote-ssh-edit
 code --install-extension ms-vscode-remote.remote-ssh-explorer
