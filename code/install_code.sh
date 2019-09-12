32db1980ec21cc69420e28f8e712373c688545f1
Jguer <me@jguer.space>
2019-09-12 13:27:55 +0100

    Switched to maim

--- code/install_code.sh
+++ code/install_code.sh
@@ -9,6 +9,7 @@ code --install-extension felipecaputo.git-project-manager
 code --install-extension foxundermoon.shell-format
 code --install-extension Gruntfuggly.todo-tree
 code --install-extension James-Yu.latex-workshop
+code --install-extension Koihik.vscode-lua-format
 code --install-extension mikestead.dotenv
 code --install-extension ms-azuretools.vscode-docker
 code --install-extension ms-python.python
@@ -18,6 +19,7 @@ code --install-extension ms-vscode-remote.remote-ssh-edit
 code --install-extension ms-vscode-remote.remote-ssh-explorer
 code --install-extension ms-vscode.cpptools
 code --install-extension ms-vscode.Go
+code --install-extension naumovs.color-highlight
 code --install-extension oderwat.indent-rainbow
 code --install-extension PKief.material-icon-theme
 code --install-extension streetsidesoftware.code-spell-checker
