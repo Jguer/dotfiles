167bac7f6fb682c17e63eaed338caf8ce60180da
jguerreiro <joao.guerreiro@gitguardian.com>
2020-03-04 18:03:52 +0100

    feat(dotfiles): change wallpaper,xresources and vscode theme

--- code/install_code.sh
+++ code/install_code.sh
@@ -2,26 +2,34 @@
 # Generated using
 # code --list-extensions | xargs -L 1 echo code --install-extension
 
+code --install-extension 42Crunch.vscode-openapi
 code --install-extension ahmadawais.shades-of-purple
 code --install-extension bierner.markdown-preview-github-styles
+code --install-extension bungcip.better-toml
 code --install-extension christian-kohler.path-intellisense
+code --install-extension eamodio.gitlens
 code --install-extension felipecaputo.git-project-manager
 code --install-extension foxundermoon.shell-format
 code --install-extension Gruntfuggly.todo-tree
 code --install-extension James-Yu.latex-workshop
 code --install-extension Koihik.vscode-lua-format
+code --install-extension liviuschera.noctis
+code --install-extension mermade.openapi-lint
+code --install-extension mhutchie.git-graph
 code --install-extension mikestead.dotenv
 code --install-extension ms-azuretools.vscode-docker
 code --install-extension ms-python.python
 code --install-extension ms-vscode-remote.remote-containers
 code --install-extension ms-vscode-remote.remote-ssh
 code --install-extension ms-vscode-remote.remote-ssh-edit
-code --install-extension ms-vscode-remote.remote-ssh-explorer
 code --install-extension ms-vscode.cpptools
 code --install-extension ms-vscode.Go
 code --install-extension naumovs.color-highlight
 code --install-extension oderwat.indent-rainbow
 code --install-extension PKief.material-icon-theme
+code --install-extension redhat.vscode-yaml
+code --install-extension RobbOwen.synthwave-vscode
 code --install-extension streetsidesoftware.code-spell-checker
 code --install-extension sumneko.lua
+code --install-extension teabyii.ayu
 code --install-extension vscodevim.vim
