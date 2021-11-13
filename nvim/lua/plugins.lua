9bbbe70ed7e584383f056808a66871deaf4064d2
jguer <me@jguer.space>
2021-11-13 12:13:20 +0100

    chore: lint new config

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,86 +1,87 @@
 local fn = vim.fn
 local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-	packer_bootstrap = fn.system({
-		"git",
-		"clone",
-		"--depth",
-		"1",
-		"https://github.com/wbthomason/packer.nvim",
-		install_path,
-	})
+  packer_bootstrap = fn.system({
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  })
 end
 
 require("packer").startup(function()
-	use("wbthomason/packer.nvim")
-	use("folke/tokyonight.nvim")
+  use("wbthomason/packer.nvim")
+  use("folke/tokyonight.nvim")
 
-	use({
-		"nvim-lualine/lualine.nvim",
-		requires = {
-			"kyazdani42/nvim-web-devicons",
-			opt = true,
-		},
-		config = function()
-			require("lualine").setup({
-				options = { theme = "tokyonight" },
-			})
-		end,
-	})
-
-	use({
-		"nvim-treesitter/nvim-treesitter",
-		run = ":TSUpdate",
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				highlight = {
-					enable = true,
-					disable = {},
-				},
-				indent = {
-					enable = true,
-					disable = {},
-				},
-				ensure_installed = {
-					"toml",
-					"json",
-					"fish",
-					"bash",
-					"go",
-					"lua",
-					"python",
-				},
-			})
-		end,
-	})
-
-	use({
-		"blackCauldron7/surround.nvim",
-		config = function()
-			require("surround").setup({ mappings_style = "surround" })
-		end,
-	})
-
-	use({
-		"lewis6991/gitsigns.nvim",
-		requires = { "nvim-lua/plenary.nvim" },
-		config = function()
-			require("gitsigns").setup()
-		end,
-	})
-
-	use({"lukas-reineke/indent-blankline.nvim",
-		config = function()
-      require("indent_blankline").setup({
-          show_current_context = true,
-          show_current_context_start = true,
+  use({
+    "nvim-lualine/lualine.nvim",
+    requires = {
+      "kyazdani42/nvim-web-devicons",
+      opt = true,
+    },
+    config = function()
+      require("lualine").setup({
+        options = { theme = "tokyonight" },
       })
-		end,
-	})
+    end,
+  })
 
-	use("b3nj5m1n/kommentary")
+  use({
+    "nvim-treesitter/nvim-treesitter",
+    run = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        highlight = {
+          enable = true,
+          disable = {},
+        },
+        indent = {
+          enable = true,
+          disable = {},
+        },
+        ensure_installed = {
+          "toml",
+          "json",
+          "fish",
+          "bash",
+          "go",
+          "lua",
+          "python",
+        },
+      })
+    end,
+  })
 
-	if packer_bootstrap then
-		require("packer").sync()
-	end
+  use({
+    "blackCauldron7/surround.nvim",
+    config = function()
+      require("surround").setup({ mappings_style = "surround" })
+    end,
+  })
+
+  use({
+    "lewis6991/gitsigns.nvim",
+    requires = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("gitsigns").setup()
+    end,
+  })
+
+  use({
+    "lukas-reineke/indent-blankline.nvim",
+    config = function()
+      require("indent_blankline").setup({
+        show_current_context = true,
+        show_current_context_start = true,
+      })
+    end,
+  })
+
+  use("b3nj5m1n/kommentary")
+
+  if packer_bootstrap then
+    require("packer").sync()
+  end
 end)
