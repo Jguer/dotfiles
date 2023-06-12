fc4bfc91a846f6a87773741885fe77851df4f671
jguer <me@jguer.space>
2023-06-12 13:53:31 +0200

    code

--- first-run.sh
+++ first-run.sh
@@ -13,13 +13,23 @@ fc-cache
 
 # Code extensions
 # code --list-extensions | xargs -L 1 echo code --install-extension
-code --install-extension eamodio.gitlens
-code --install-extension enkia.tokyo-night
-code --install-extension GitHub.copilot
-code --install-extension golang.go
-code --install-extension ms-vsliveshare.vsliveshare
-code --install-extension PKief.material-icon-theme
-code --install-extension vscodevim.vim
+code-insiders --install-extension --pre-release eamodio.gitlens
+code-insiders --install-extension --pre-release enkia.tokyo-night
+code-insiders --install-extension --pre-release GitHub.copilot-chat
+code-insiders --install-extension --pre-release GitHub.copilot-labs
+code-insiders --install-extension --pre-release GitHub.copilot-nightly
+code-insiders --install-extension --pre-release golang.go
+code-insiders --install-extension --pre-release hashicorp.terraform
+code-insiders --install-extension --pre-release ms-azuretools.vscode-docker
+code-insiders --install-extension --pre-release ms-kubernetes-tools.vscode-kubernetes-tools
+code-insiders --install-extension --pre-release ms-python.python
+code-insiders --install-extension --pre-release ms-python.vscode-pylance
+code-insiders --install-extension --pre-release ms-vsliveshare.vsliveshare
+code-insiders --install-extension --pre-release PKief.material-icon-theme
+code-insiders --install-extension --pre-release redhat.vscode-yaml
+code-insiders --install-extension --pre-release streetsidesoftware.code-spell-checker
+code-insiders --install-extension --pre-release tilt-dev.tiltfile
+code-insiders --install-extension --pre-release vscodevim.vim
 
-gsettings set org.gnome.desktop.interface icon-theme 'dracula'
-gsettings set org.gnome.desktop.interface gtk-theme 'dracula'
+gsettings set org.gnome.desktop.interface icon-theme 'Tokyonight-Dark-Cyan'
+gsettings set org.gnome.desktop.interface gtk-theme 'Tokyonight-Storm'
