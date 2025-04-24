984316b80e8ddbdb730f53c3207384d4fa021b08
Jo Garnier <git@jguer.space>
2025-04-24 14:06:43 +0200

    greeting

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -8,6 +8,7 @@ fish_add_path --append /home/jguer/go/bin
 if status is-interactive
   starship init fish | source
   zoxide init fish | source
+  set fish_greeting ""
   set -e SSH_AGENT_PID
   if not set -q gnupg_SSH_AUTH_SOCK_by; or test "$gnupg_SSH_AUTH_SOCK_by" -ne "$fish_pid"
     set -gx SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
