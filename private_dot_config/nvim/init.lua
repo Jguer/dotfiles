0cc66648593851b636a2ab91704d6f8dcb6dc317
Jo Garnier <me@jguer.space>
2026-07-06 13:15:17 +0200

    fix ssh nvim

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -22,6 +22,18 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 vim.opt.clipboard = "unnamedplus"
 
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+  },
+}
+
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
